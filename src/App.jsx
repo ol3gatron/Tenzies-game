c43e63983b6e66caebe0ae3c6fe61681a788beef
ol3gatron <olegatron@gmail.com>
2022-08-06 19:17:39 +0300

    added a button to roll dices

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,35 @@ import { useState } from "react"
 import Die from "./components/Die"
 
 function App() {
-  const [diceArray, setDiceArray] = useState(() => allNewDice(1, 6))
+  const [diceArray, setDiceArray] = useState(allNewDice())
 
-  function allNewDice(max, min) {
+  function allNewDice() {
     const array = []
+
+    function createDiceObject() {
+      const value = Math.ceil(Math.random() * 6)
+      const isHeld = false
+      return ({value, isHeld})
+    }
+
     for (let i = 0; i < 10; i++) {
-      array.push(Math.floor(Math.random() * (max, min) + 1))
+      array.push(createDiceObject())
     }
     return array
   }
 
   console.log(diceArray)
 
+  function handleClick() {
+    setDiceArray(() => allNewDice())
+  }
+
   return (
     <main>
       <div className="container">
-        {diceArray.map((die) => <Die value={die}/>)}
+        {diceArray.map((die, index) => <Die value={die.value} isHeld={die.isHeld} key={index}/>)}
       </div>
+      <button className="roll-btn" onClick={handleClick}>Roll</button>
     </main>
   )
 }
