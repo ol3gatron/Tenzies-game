7d6005fc0f95354586db780c6a27b6e711e3d989
ol3gatron <olegatron@gmail.com>
2022-08-07 22:30:04 +0300

    dices now styled with grid

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,25 +1,18 @@
 function Die(props) {
   const style = {
     backgroundColor: props.isHeld ? "#59E391" : "#FFF",
-    borderRadius: "3px",
-    cursor: "pointer",
-    userSelect: "none",
-
-    width: "45px",
-    height: "45px",
-    position: "relative",
-    boxShadow: "0 2px 2px #00000026",
-    margin: "5px",
   }
 
 
   return (
     <div
       style={style}
-      className={props.value !== 6 ? `die die-${props.value}` : "die-6"}
+      className={`die die-${props.value}`}
       onClick={props.holdDice}
     >
-      {props.value === 1 && <span className="die-1--first"></span>}
+      {props.value === 1 &&
+        <span className="die-1--first"></span>
+      }
       {props.value === 2 &&
         <>
           <span className="die-2--first"></span>
@@ -51,14 +44,14 @@ function Die(props) {
         </>
       }
       {props.value === 6 &&
-        <div className="die-6-container">
+        <>
           <span className="die-6--first"></span>
           <span className="die-6--second"></span>
           <span className="die-6--third"></span>
           <span className="die-6--fourth"></span>
           <span className="die-6--fifth"></span>
           <span className="die-6--sixth"></span>
-        </div>
+        </>
       }
     </div>
   )
